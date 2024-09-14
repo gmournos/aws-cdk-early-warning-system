a7e3bed6bc65504c7bef40318380cb7d0c7e4016
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 05:53:59 +0200

    Add empty log error alerts stack

--- lib/aws-cdk-early-warning-system-stack.ts
+++ lib/aws-cdk-early-warning-system-stack.ts
@@ -1,10 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { LogGroupRetentionStack } from './features/log-group-retention';
+import { LogGroupErrorAlertsStack } from './features/log-group-error-alerts';
 
 export class AwsCdkEarlyWarningSystemStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
     new LogGroupRetentionStack(this, 'log-group-retention-stack', props);
+    new LogGroupErrorAlertsStack(this, 'log-group-error-alerts-stack', props);
+
   }
 }
