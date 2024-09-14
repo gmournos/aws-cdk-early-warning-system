c9a103efca5d0ac8ecd7f71e7bea039452cb57f2
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 06:29:42 +0200

    Create lambda function that sends alerts if error is reported by subscription filter

---
+++ lambda/subscription-filters/handlers.ts
@@ -0,0 +1,20 @@
+import { CloudWatchLogsEvent } from "aws-lambda";
+import { sendCustomizedNotificationFromLogGroupSubscription } from "./utils";
+
+const stripLogGroupName = (logGroupName : string) => logGroupName.replace('/aws/lambda/', '');
+
+export const sendCustomizedNotificationFromErrorLogSubscription = async (event: CloudWatchLogsEvent) => {
+    console.info('sending notification for failure appearing in Log group');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const makeSubjectFunc = (logGroup: string, _: string) => `Error Alarm for ${stripLogGroupName(logGroup)}`;
+
+    const makeBodyFragmentFunc = (logRecord: string) => {
+        const accountEnvironment = process.env.ACCOUNT_ENVIRONMENT;
+
+        const shortMessage = `Error in logs at ${logRecord.substring(0, Math.min(logRecord.length, 60))}...`; 
+        return accountEnvironment === 'PRODUCTION' ? shortMessage : logRecord; 
+    };
+
+    await sendCustomizedNotificationFromLogGroupSubscription(event, makeBodyFragmentFunc, makeSubjectFunc);
+};
