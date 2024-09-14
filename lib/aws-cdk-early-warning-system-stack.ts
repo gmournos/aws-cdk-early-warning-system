93f31f1d0255b57982f764b6c9465a1cfd873a57
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 05:57:04 +0200

    Import an alerts topic

--- lib/aws-cdk-early-warning-system-stack.ts
+++ lib/aws-cdk-early-warning-system-stack.ts
@@ -2,13 +2,21 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { LogGroupRetentionStack } from './features/log-group-retention';
 import { LogGroupErrorAlertsStack } from './features/log-group-error-alerts';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 
 export class AwsCdkEarlyWarningSystemStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const topic = new Topic(this, 'alerts-topic', {
+      topicName : 'EarlyWarningSystemAlerts',
+    });
     
     new LogGroupRetentionStack(this, 'log-group-retention-stack', props);
-    new LogGroupErrorAlertsStack(this, 'log-group-error-alerts-stack', props);
+    new LogGroupErrorAlertsStack(this, 'log-group-error-alerts-stack', {
+      ...props,
+      destinationTopic: topic
+    });
 
   }
 }
