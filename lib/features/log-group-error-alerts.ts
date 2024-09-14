cd46c136581cd41aee4ba2ff4b3c6a543b12538c
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 06:48:01 +0200

    Create the deployment of log group subscription filter alerter lambda function

--- lib/features/log-group-error-alerts.ts
+++ lib/features/log-group-error-alerts.ts
@@ -1,15 +1,29 @@
 import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
 import { ITopic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
+import { createLogSubscriptionAlertFunction } from '../utils/cloudwatch';
 
 export interface LogGroupErrorAlertsStackProps extends NestedStackProps {
     destinationTopic: ITopic;
     accountEnvironment: string;
 }
 
+const FUNCTION_NAME = 'alert-for-log-group-error-function';
+
 export class LogGroupErrorAlertsStack extends NestedStack {
 
     constructor(scope: Construct, id: string, props: LogGroupErrorAlertsStackProps) {
         super(scope, id, props);
+    }
+
+    createLogErrorSubcriptionFunction(accountEnvironment: string, destinationTopic: ITopic) {
+        return createLogSubscriptionAlertFunction({
+            scope: this,
+            functionName: FUNCTION_NAME,
+            accountEnvironment,
+            handler: 'sendCustomizedNotificationFromErrorLogSubscription',
+            sourceFilePath: ['subscription-filters', 'handlers.ts'],
+            destinationTopic,
+        });
     }
 }
