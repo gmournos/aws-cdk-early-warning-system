c12ce8d0d49540608e3e136835a35758a88bd2c0
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 05:25:29 +0200

    Read default retention from parameter store

--- lib/features/log-group-retention.ts
+++ lib/features/log-group-retention.ts
@@ -1,9 +1,12 @@
 import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
 export class LogGroupRetentionStack extends NestedStack {
 
     constructor(scope: Construct, id: string, props?: NestedStackProps) {
         super(scope, id, props);
+        const defaultRetentionParam = StringParameter.valueForStringParameter(this, '/ews/cloudwatch/logs/retetion');
+        const defaultRetention = parseFloat(defaultRetentionParam);
     }
 }
