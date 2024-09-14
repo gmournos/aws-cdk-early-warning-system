d83ed055e0d7c2206851a81caf19944057ae8c52
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 07:56:50 +0200

    Event bridge rule for etl failures

--- lib/features/glue-etl-failures.ts
+++ lib/features/glue-etl-failures.ts
@@ -7,6 +7,8 @@ import { ILogGroup } from 'aws-cdk-lib/aws-logs';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
+import { Rule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 
 export interface GlueJobFailuresStackProps extends NestedStackProps {
     destinationTopic: ITopic;
@@ -23,6 +25,7 @@ export class GlueJobFailuresStack extends NestedStack {
         super(scope, id, props);
         this.logGroup = buildLogGroupForLambda(this, FUNCTION_NAME);
         this.failedEtlFunction = this.createFailedEtlLambdaFunction(props.accountEnvironment, props.destinationTopic);
+        this.createErrorRule();
     }
 
     createFailedEtlLambdaFunction(accountEnvironment: string, destinationTopic: ITopic) {
@@ -37,7 +40,7 @@ export class GlueJobFailuresStack extends NestedStack {
             ],
         });
 
-        const sendCustomizedEmailFromEtlFailedEvent = new NodejsFunction(this, 'failed-etl-function', {
+        const sendCustomizedNotificationFromEtlFailedEvent = new NodejsFunction(this, 'failed-etl-function', {
             functionName: FUNCTION_NAME,
             logGroup: this.logGroup,
             runtime: Runtime.NODEJS_20_X,
@@ -49,7 +52,23 @@ export class GlueJobFailuresStack extends NestedStack {
             },
         });
 
-        sendCustomizedEmailFromEtlFailedEvent.addToRolePolicy(sendToTopicPolicy);
-        return sendCustomizedEmailFromEtlFailedEvent;
+        sendCustomizedNotificationFromEtlFailedEvent.addToRolePolicy(sendToTopicPolicy);
+        return sendCustomizedNotificationFromEtlFailedEvent;
+    }
+
+    createErrorRule() {
+
+        // Define the EventBridge rule
+        const rule = new Rule(this, 'etl-fail-rule', {
+            ruleName: 'etl-fail-rule',
+            eventPattern: {
+                source: ['aws.glue'],
+                detailType: ['Glue Job State Change'],
+                detail: {
+                    state: ['FAILED'],
+                },
+            },
+        });
+        rule.addTarget(new LambdaFunction(this.failedEtlFunction)); // Replace yourLambdaFunction with your Lambda function
     }
 }
